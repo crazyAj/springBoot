daee88c388e80655fb0d109185c5c93d639e6508
crazyAj <yangwenjiemail@163.com>
2020-05-20 09:16:46 +0800

    统一本地环境

--- src/main/java/com/example/demo/Testy.java
+++ src/main/java/com/example/demo/Testy.java
@@ -1,11 +1,7 @@
 package com.example.demo;
 
-import com.example.demo.utils.cryptology.Md5Encrypt;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
-
-import java.math.BigDecimal;
-import java.util.UUID;
 
 @Slf4j
 //@RunWith(SpringRunner.class)
@@ -18,7 +14,6 @@ public class Testy {
 
     @Test
     public void t() {
-        System.out.println();
     }
 
     @Test
